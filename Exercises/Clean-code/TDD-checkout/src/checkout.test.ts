a8b08c67e158a31ecd9f45c2af70d18011c37987
Florence Navet <enolaworld@gmail.com>
2026-09-22 20:32:55 +0200

    feat(checkout): price Eggs and Apples in tdd

--- Exercises/Clean-code/TDD-checkout/src/checkout.test.ts
+++ Exercises/Clean-code/TDD-checkout/src/checkout.test.ts
@@ -1,35 +1,108 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from "vitest";
 
-import { Checkout } from './checkout'
+import { Checkout } from "./checkout";
 
+describe("checkout kata", () => {
+  it("runs the test suite", () => {
+    expect(true).toBe(true);
+  });
+});
 
-describe('checkout kata', () => {
-  it('runs the test suite', () => {
-    expect(true).toBe(true)
-  })
+it("totals 0 when nothing is scanned", () => {
+  const checkout = new Checkout();
+  expect(checkout.total()).toBe(0);
+});
 
+it("total 50 for oNe apple", () => {
+  const checkout = new Checkout({ Apple: 50 });
+  checkout.scan("Apple");
+  expect(checkout.total()).toBe(50);
+});
 
-})
+it("total 100 for Two apples", () => {
+  const checkout = new Checkout({ Apple: 50 });
+  checkout.scan("Apple");
+  checkout.scan("Apple");
+  expect(checkout.total()).toBe(100);
+});
 
-  it('totals 0 when nothing is scanned', () => {
-    const checkout = new Checkout()
-    expect(checkout.total()).toBe(0)
-  })
+it("totals 20 for one egg", () => {
+  const checkout = new Checkout({ Egg: 20 });
+  checkout.scan("Egg");
+  expect(checkout.total()).toBe(20);
+});
 
+it("totals 40 for two eggs", () => {
+  const checkout = new Checkout();
 
-    it('total 50 for oNe apple', () => {
-    const checkout = new Checkout({Apple : 50})
-    checkout.scan('Apple')
-    expect(checkout.total()).toBe(50)
-  })
+  checkout.scan("Egg");
+  checkout.scan("Egg");
 
-      it('total 100 for Two apples', () => {
-    const checkout = new Checkout({Apple : 100})
-    checkout.scan('Apple')
-    checkout.scan('Apple')
-    expect(checkout.total()).toBe(100)
-  })
+  expect(checkout.total()).toBe(40);
+});
 
+it("totals 70 for one apple and one egg", () => {
+  const checkout = new Checkout();
+
+  checkout.scan("Apple");
+  checkout.scan("Egg");
+
+  expect(checkout.total()).toBe(70);
+});
+
+it("totals 130 for three apples", () => {
+  const checkout = new Checkout();
+
+  checkout.scan("Apple");
+  checkout.scan("Apple");
+  checkout.scan("Apple");
+
+  expect(checkout.total()).toBe(130);
+});
+
+it("totals 30 for one carrot", () => {
+  const checkout = new Checkout();
+
+  checkout.scan("Carrot");
+
+  expect(checkout.total()).toBe(30);
+});
+
+it("totals 45 for two carrots", () => {
+  const checkout = new Checkout();
+
+  checkout.scan("Carrot");
+  checkout.scan("Carrot");
+
+  expect(checkout.total()).toBe(45);
+});
+
+it("totals 40 for two eggs", () => {
+  const checkout = new Checkout();
+
+  checkout.scan("Egg");
+  checkout.scan("Egg");
+
+  expect(checkout.total()).toBe(40);
+});
+
+it("totals 40 for two eggs", () => {
+  const checkout = new Checkout();
+
+  checkout.scan("Egg");
+  checkout.scan("Egg");
+
+  expect(checkout.total()).toBe(40);
+});
+
+it("totals 15 for two Yoghourt", () => {
+  const checkout = new Checkout();
+
+  checkout.scan("Yoghourt");
+  checkout.scan("Yoghourt");
+
+  expect(checkout.total()).toBe(30);
+});
 
 // caisse vide 6 -> 0
 // un item pomme 6 -> 50
@@ -38,5 +111,5 @@ describe('checkout kata', () => {
 // 2 oeufs -> 40
 // 1 pommme + 1 oeuf -> 30
 // 3 pommes -> 130
-// 
-// 
+//
+//
--- Exercises/Clean-code/TDD-checkout/src/checkout.ts
+++ Exercises/Clean-code/TDD-checkout/src/checkout.ts
@@ -1,14 +1,49 @@
 export class Checkout {
-//   private scanned = false
-private count = 0
+  //   private scanned = false
+  private amount = 0;
+  private appleCount = 0;
+  private carrotCount = 0;
+
+  private applyApplePromotion() {
+    if (this.appleCount === 3) {
+      this.amount -= 20;
+    }
+  }
+
+  private applyCarrotPromotion() {
+    if (this.carrotCount === 2) {
+      this.amount -= 15;
+    }
+  }
 
   scan(name: string) {
+    if (name === "Egg") {
+      this.amount += 20;
+      return;
+    }
+    if (name === "Yoghourt") {
+      this.amount += 15;
+      return;
+    }
+    if (name === "Carrot") {
+      this.carrotCount++;
+      this.amount += 30;
+      this.applyCarrotPromotion();
+      return;
+    }
+    if (name === "Apple") {
+      this.appleCount++;
+      this.amount += 50;
+      this.applyApplePromotion();
+      return;
+    }
+
     // this.scanned = true
-    this.count++;
   }
 
   total() {
     // if (this.scanned) return 50
-    return this.count * 50;
+    // return this.count * 50;
+    return this.amount;
   }
 }
